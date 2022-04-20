6771fa361af80bcd7d5eefb247acb7d31db89dc0
tanyue <paojiao1224@163.com>
2022-04-20 17:31:45 +0800

    提交

--- src/main/java/com/example/demo/common/practice/controller/EmployeeController.java
+++ src/main/java/com/example/demo/common/practice/controller/EmployeeController.java
@@ -2,8 +2,10 @@ package com.example.demo.common.practice.controller;
 
 import com.example.demo.common.entity.Result;
 import com.example.demo.common.practice.dto.EmployeeDTO;
+import com.example.demo.common.practice.service.DailyService;
 import com.example.demo.common.practice.service.impl.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,17 @@ public class EmployeeController
 {
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private DailyService dailyService;
     @PutMapping("/saveEmployee")
     public Result saveEmployee(@RequestBody EmployeeDTO employeeDTO){
         employeeService.saveEmployee(employeeDTO.getEmployee());
+        return new Result().success();
+    }
+
+    @GetMapping("/testSync")
+    public Result testSync(){
+        dailyService.syncCal();
         return new Result().success();
     }
 }
--- src/main/java/com/example/demo/common/practice/service/DailyService.java
+++ src/main/java/com/example/demo/common/practice/service/DailyService.java
@@ -3,4 +3,7 @@ package com.example.demo.common.practice.service;
 public interface DailyService {
     //根据城市代码获取天气信息
     String getWeatherInfo(String cityDm);
+
+    void syncCal();
+
 }
